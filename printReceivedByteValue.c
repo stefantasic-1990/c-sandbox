bf51807362b237140d48f8fb3f2c2fb71738aa28
Stefan Tasic <stefan.tasic@points.com>
2024-11-25 18:12:22 -0500

    add little program to print received byte values

---
+++ printReceivedByteValue.c
@@ -0,0 +1,22 @@
+/*
+    A little program to print the integer values of the bytes received through terminal input.
+    When you run this program, the terminal will buffer the input until you press enter.
+    This program will then print out the integer values of each byte received in order including the newline.
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+
+    do {
+        char c;
+        ssize_t bytes_read;
+
+        // read a single byte from standard input
+        bytes_read = read(STDIN_FILENO, &c, 1);
+
+        // print the int value of the received character
+        printf("Received byte int value: %d\n", c);
+    } while (1);
+}

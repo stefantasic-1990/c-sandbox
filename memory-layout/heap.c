bfb37c39bbf97d36ff0d71ea2c18ddb4dae43134
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-30 07:37:24 -0400

    some heap and allocator observation

---
+++ memory-layout/heap.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void print_break(void)
+{
+    void *brk = sbrk(0);
+    printf("The program break address is: %p\n", brk);
+}
+
+void *allocate(int bytes)
+{
+    void *ptr = malloc(bytes);
+    printf("\nAllocated %i bytes of memory starting at address: %p\n", bytes, ptr);
+    print_break();
+
+    return ptr;
+}
+
+void *release(void *ptr)
+{
+    free(ptr);
+    printf("\nFreed memory starting at address: %p\n", ptr);
+    print_break();
+}
+
+int main(void)
+{
+    print_break();
+    void *a = allocate(1);
+    void *b = allocate(30);
+    void *c = allocate(24);
+    void *d = allocate(8);
+    release(a);
+    void *e = allocate(12);
+}
